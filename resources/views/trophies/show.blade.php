42c37dcbda2444f9dbf05735434c8e1d82352a6b
Tahazzat-dev <mdtahazzatali820@gmail.com>
2026-07-08 10:02:33 +0600

    updated everything

--- resources/views/trophies/show.blade.php
+++ resources/views/trophies/show.blade.php
@@ -6,8 +6,10 @@
      <div class="pt-5 lg:pt-0 w-full">
        <div class="w-full mb-5 flex px-4 items-center">
         <button
-         class="md:hidden"
-      >
+            type="button"
+            onclick="if (window.history.length > 1) { window.history.back(); } else { window.location.href = '{{ route('trophies.all', $category) }}'; }"
+            class="md:hidden"
+        >
         <svg  class="min-w-5 size-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M73.4 297.4C60.9 309.9 60.9 330.2 73.4 342.7L233.4 502.7C245.9 515.2 266.2 515.2 278.7 502.7C291.2 490.2 291.2 469.9 278.7 457.4L173.3 352L544 352C561.7 352 576 337.7 576 320C576 302.3 561.7 288 544 288L173.3 288L278.7 182.6C291.2 170.1 291.2 149.8 278.7 137.3C266.2 124.8 245.9 124.8 233.4 137.3L73.4 297.3z"/></svg>
       </button>
        <div class=" inline-block w-fit mx-auto text-md h-auto rounded-full bg-primary px-10 py-2 font-bold text-white shadow-sm">
